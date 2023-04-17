ea24a6c19484005a226fddd9d8e6cdaece27498e
Fausio Matsinhe <fausio.matsinhe@kooli.io>
2023-04-17 09:48:39 +0200

    clean again

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { MatSlideToggleModule } from '@angular/material/slide-toggle'; 
-import { MatDrawer } from '@angular/material/sidenav';
-import { MatDrawerContainer } from '@angular/material/sidenav'; 
+import { MatToolbarModule } from '@angular/material/toolbar'; 
 import { MatSidenavModule } from '@angular/material/sidenav'; 
+import { MatIconModule } from '@angular/material/icon'; 
+import { MatListModule } from '@angular/material/list'; 
+import { MatButtonModule } from '@angular/material/button'; 
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavBarComponent } from './component/menu/nav-bar/nav-bar.component';
-import { SideBarComponent } from './component/menu/side-bar/side-bar.component';
+import { AppComponent } from './app.component';  
+import { SideNavComponent } from './component/menu/side-nav/side-nav.component';
+import { BodyComponent } from './component/body/body.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavBarComponent,
-    SideBarComponent
+    AppComponent,  
+    SideNavComponent, BodyComponent, DashboardComponent
   ],
   imports: [
     BrowserModule,
@@ -25,7 +27,8 @@ import { SideBarComponent } from './component/menu/side-bar/side-bar.component';
 
     MatSlideToggleModule,
     MatSidenavModule, 
-    
+    MatToolbarModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]

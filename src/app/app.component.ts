ac0ac0de3cae41845e45df0ee1aa8cacf8291efe
Fausio Matsinhe <fausio.matsinhe@kooli.io>
2023-04-17 11:39:53 +0200

    event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,14 @@
 import { Component } from '@angular/core';
 
+
+interface SideNavTogle {
+
+  screenWidth: number;
+  collapsed: boolean;
+
+}
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +16,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'csi.angular.fron';
+
+  isSideNavCollapsed = false;
+  screenWidth= 0;
+
+  onTogleSideNav(data: SideNavTogle): void {
+
+    this.screenWidth = data.screenWidth;
+    this.isSideNavCollapsed = data.collapsed;
+  }
 }
--- src/app/component/side-nav/side-nav.component.ts
+++ src/app/component/side-nav/side-nav.component.ts
@@ -1,5 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { navbarData } from './nav-data';
+
+
+interface SideNavTogle {
+
+  screenWidth: number;
+  collapsed: boolean;
+
+}
 
 @Component({
   selector: 'app-side-nav',
@@ -8,17 +16,29 @@ import { navbarData } from './nav-data';
 })
 export class SideNavComponent {
 
+  @Output() onTogleSideNav: EventEmitter<SideNavTogle> = new EventEmitter();
 
   collapse = true;
+  screenWidth = 0;
   navDate = navbarData;
 
 
   toggleCollapse(): void {
 
-    this.collapse = !  this.collapse ;
+    this.collapse = !this.collapse;
+    this.onTogleSideNav.emit(
+      {
+        collapsed: this.collapse,
+        screenWidth: this.screenWidth
+      })
   }
   closseToggleCollapse(): void {
 
     this.collapse = false;
+    this.onTogleSideNav.emit(
+      {
+        collapsed: this.collapse,
+        screenWidth: this.screenWidth
+      })
   }
 }
